eace5fa82e3f43e7fbc9d2678b06f5b862367202
wangmeng <wangmeng@i72.com>
2024-01-23 17:10:21 +0800

    feat: config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
   images: {
     domains: [
-      "gpts-works.s3.us-west-1.amazonaws.com",
-      "trysai.s3.us-west-1.amazonaws.com",
+      'gpts-works.s3.us-west-1.amazonaws.com',
+      'trysai.s3.us-west-1.amazonaws.com',
+      'robinawstest001.s3.ap-southeast-1.amazonaws.com',
     ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
